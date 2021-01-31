3c0b22589028179a33a3fc56c682aeb83f2e6e7f
zzcandor <zzcandor@gmail.com>
2021-01-31 18:17:38 +0800

    1 file modified

--- src/components/UseMemoDemo.js
+++ src/components/UseMemoDemo.js
@@ -1,8 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, memo, useMemo } from "react";
 
 // 子组件
-function Child({ userInfo }) {
-  console.log("Child render...", userInfo);
+// function Child({ userInfo }) {
+//   console.log("Child render...", userInfo);
+//   return (
+//     <div>
+//       <p>
+//         This is Child {userInfo.name} {userInfo.age}
+//       </p>
+//     </div>
+//   );
+// }
+
+// 类似于Class 组件PureComponent, 对props 进行浅层对比
+// useMemo返回值：值 useCallBack 返回值：函数
+const Child = memo(({ userInfo }) => {
+  console.log("Child render", userInfo);
   return (
     <div>
       <p>
@@ -10,7 +23,7 @@ function Child({ userInfo }) {
       </p>
     </div>
   );
-}
+});
 
 // 父组件
 function App() {
@@ -18,10 +31,13 @@ function App() {
   const [count, setCount] = useState(0);
   const [name, setName] = useState("小牛");
 
-  const userInfo = {
-    name,
-    age: 20
-  };
+  // const userInfo = {
+  //   name,
+  //   age: 20
+  // };
+  const userInfo = useMemo(() => {
+    return { name, age: 21 };
+  }, [name]);
 
   return (
     <div>
