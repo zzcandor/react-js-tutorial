47a7461984d120bdfc2690dcbb15b6cd7044a3e6
zzcandor <zzcandor@gmail.com>
2021-01-31 17:12:19 +0800

    1 file modified

--- src/components/UseReducerDemo.js
+++ src/components/UseReducerDemo.js
@@ -13,6 +13,9 @@ const reducer = (state, action) => {
   }
 };
 
+// useReducer 是 useState 的代替方案，用于state复杂变化
+// useReducer 是个单个组件状态管理，组件通讯还需要pros
+// redux 是全局的状态管理，多组件共享数据
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
   return (
