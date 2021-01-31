502a88e0d5b4ac8607e7ab0c3aca12f8284cceb6
zzcandor <zzcandor@gmail.com>
2021-01-31 18:38:34 +0800

    1 file modified, 2 added

---
+++ src/customHooks/useAxios.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+// 封装 axios 发送网络请求的自定义 Hook
+function useAxios(url) {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState();
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(url)
+      .then(res => setData(res))
+      .catch(err => setError(err))
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [url]);
+
+  return [loading, data, error];
+}
